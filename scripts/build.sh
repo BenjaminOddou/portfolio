d9b927f76a79b9f1e25785b1f0b1ccee5ca2c404
Benjamin Oddou <benjaminoddou@gmail.com>
2023-10-22 22:28:00 +0200

    test: git config

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,3 +15,6 @@ cat ~/.ssh/id_ed25519
 # Add host keys, comment out if not needed
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 # ssh-keyscan -H bitbucket.org >> ~/.ssh/known_hosts
+
+git config core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+npm run build

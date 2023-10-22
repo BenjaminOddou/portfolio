ddc4a426da23ce3e3932b4a14260b2cb418184ea
Benjamin Oddou <benjaminoddou@gmail.com>
2023-10-22 22:31:13 +0200

    test: preinstall hook

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,4 +17,3 @@ ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 # ssh-keyscan -H bitbucket.org >> ~/.ssh/known_hosts
 
 git config core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
-npm run build

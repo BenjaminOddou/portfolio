f58bc86259a110760dc68e7eab5e923c4e729394
Benjamin Oddou <benjaminoddou@gmail.com>
2023-10-22 15:40:01 +0200

    chore: update packages

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set up SSH key
-echo "$GITHUB_TOKEN" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-ssh -vT git@github.com
-git --no-replace-objects ls-remote ssh://git@github.com/BenjaminOddou/gsap-shockingly-green.git

921e4567a4ce831c546524242c872b3311d27e5b
Benjamin Oddou <benjaminoddou@gmail.com>
2023-10-23 22:25:21 +0200

    test: update ssh script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env bash
-
-# Check if we're running in a Netlify environment
-# See https://www.netlify.com/docs/continuous-deployment/#environment-variables
-
-# Init .ssh dir and expand $SSH_KEY
 mkdir -p ~/.ssh
 echo -e "${SSH_KEY//_/\\n}" > ~/.ssh/id_ed25519
-chmod og-rwx ~/.ssh/id_ed25519
-
-# Uncomment to debug
-ls -la ~/.ssh
-cat ~/.ssh/id_ed25519
-
-# Add host keys, comment out if not needed
+chmod 600 ~/.ssh/id_ed25519
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-# ssh-keyscan -H bitbucket.org >> ~/.ssh/known_hosts
-
-git config core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+ssh -vT git@github.com
+git --no-replace-objects ls-remote ssh://git@github.com/BenjaminOddou/gsap-shockingly-green.git

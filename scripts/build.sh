ad2945af7afa7f56c516bc5d40b489d6f7ce2a50
Benjamin Oddou <benjaminoddou@gmail.com>
2023-10-24 00:02:10 +0200

    test: github pat

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p ~/.ssh
-echo -e "${SSH_KEY//_/\\n}" > ~/.ssh/id_ed25519
-chmod 600 ~/.ssh/id_ed25519
-ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-ssh -vT git@github.com
-git --no-replace-objects ls-remote ssh://git@github.com/BenjaminOddou/gsap-shockingly-green.git

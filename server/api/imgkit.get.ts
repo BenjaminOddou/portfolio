2e770a17a261177898eb0e1639dfe63ac4c65811
Benjamin Oddou <benjaminoddou@gmail.com>
2026-03-29 23:22:25 +0200

    test: debug imgkit

--- server/api/imgkit.get.ts
+++ server/api/imgkit.get.ts
@@ -1,28 +1,43 @@
+// server/api/imgkit.get.ts
 import { Buffer } from 'node:buffer'
 
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig(event)
 
-  const privateKey = config.IMAGEKIT_API
+  const privateKey = config.imagekitApi
 
   if (!privateKey) {
-    throw createError({ statusCode: 500, statusMessage: 'Cle manquante' })
+    console.error('ERREUR : La variable imagekitApi est indefinie.')
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Configuration serveur incorrecte (Cle manquante).',
+    })
   }
 
-  const b64Token = Buffer.from(`${privateKey}:`).toString('base64')
+  try {
+    const b64Token = Buffer.from(`${privateKey}:`).toString('base64')
 
-  const response = await $fetch<ImageKit[]>('https://api.imagekit.io/v1/files', {
-    headers: {
-      Authorization: `Basic ${b64Token}`,
-    },
-  })
+    const response = await $fetch<any[]>('https://api.imagekit.io/v1/files', {
+      headers: {
+        Authorization: `Basic ${b64Token}`,
+      },
+    })
 
-  return response.map(item => ({
-    name: item.name,
-    filePath: item.filePath,
-    tags: item.tags,
-    width: item.width,
-    height: item.height,
-    url: item.url,
-  }))
+    return response.map(item => ({
+      name: item.name,
+      filePath: item.filePath,
+      tags: item.tags,
+      width: item.width,
+      height: item.height,
+      url: item.url,
+    }))
+  }
+
+  catch (error: any) {
+    console.error('Erreur API ImageKit:', error.data || error.message)
+    throw createError({
+      statusCode: error.statusCode || 500,
+      statusMessage: 'Erreur lors de la communication avec ImageKit.',
+    })
+  }
 })
